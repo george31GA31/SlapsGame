b4f75f4dcbd1ee71cf5e91585b5e6a4873ee10e7
george31GA31 <157766840+george31GA31@users.noreply.github.com>
2026-01-25 18:45:26 +0000

    multiplayer-game.js

--- multiplayer-game.js
+++ multiplayer-game.js
@@ -908,3 +908,16 @@ function updatePenaltyUI() {
     if(pBox) pBox.innerHTML = '';
     if(aBox) aBox.innerHTML = '';
 }
+// --- NEW HELPER: STRICT OVERLAP CHECK ---
+function isOverlapping(element1, element2) {
+    if (!element1 || !element2) return false;
+    const rect1 = element1.getBoundingClientRect();
+    const rect2 = element2.getBoundingClientRect();
+
+    return !(
+        rect1.right < rect2.left || 
+        rect1.left > rect2.right || 
+        rect1.bottom < rect2.top || 
+        rect1.top > rect2.bottom
+    );
+}
